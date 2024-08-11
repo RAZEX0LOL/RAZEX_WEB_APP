4c76f30b27ac064f96be961e44f992f4a1f19dd8
RAZEX0LOL <99183060+RAZEX0LOL@users.noreply.github.com>
2024-08-11 16:25:12 +0400

    add php for send on email

---
+++ email.php
@@ -0,0 +1,24 @@
+<?php
+// несколько получателей
+$to  = 'khattayev00@mail.ru' . ', ';  // обратите внимание на запятую
+
+// тема письма
+$subject = 'Письмо с моего сайта';
+
+// текст письма
+$message = 'Пользователь' . $_POST['name'] . ' отправил вам письмо:<br />' . $_POST['message'] . '<br />
+Связяться с ним можно по email <a href="mailto:' . $_POST['email'] . '">' . $_POST['email'] . '</a>'
+;
+
+// Для отправки HTML-письма должен быть установлен заголовок Content-type
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+
+// Дополнительные заголовки
+$headers .= 'To: Расул <khattayev00@mail.ru>' . "\r\n"; // Свое имя и email
+$headers .= 'From: '  . $_POST['name'] . '<' . $_POST['email'] . '>' . "\r\n";
+
+
+// Отправляем
+mail($to, $subject, $message, $headers);
+?>
